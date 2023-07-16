328da85ebb38fe57ae4353c028f57e1c74a138ae
Ali Taş <alitas2400@gmail.com>
2023-07-16 15:14:40 +0300

    add link to alitas.dev website

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex h-full min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-2xl tracking-tight">Next.js Starter Template</h1>
+      <Link href={"https://alitas.dev"} className="text-xl font-extralight">
+        @AliTas
+      </Link>
     </main>
   );
 }
